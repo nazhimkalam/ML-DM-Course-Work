b68fff44d1aa4a154cc85e7f42ad972bc8c27428
Nazhim <nazhimkalam@gmail.com>
2021-02-25 16:34:04 +0530

    completed removing outliers from the dataset

--- w1761265.R
+++ w1761265.R
@@ -13,44 +13,90 @@ install.packages("readxl")
 library("readxl")
 
 # Reading the data-set "vehicles.xlsx"
-df = read_excel(file.choose())
+df = read_excel("D:/IIT/2nd Year/Data Mining & Machine Leanring/Coursework/vehicles.xlsx")
+View(df)
+
+# Removing the Sample index column and the class column from the dataset
+df.filtered = subset(df, select = -c(Samples, Class))
 
 # Viewing the head of the data-set
-View(df)
+View(df.filtered)
 
 # [PRE-PROCESSING DATA] PERFORMING SCALING AND OUTLIERS REMOVAL
 
 # Checking for any null values present in the data-set (returned 0 so no null values present)
-print(sum(is.na(df)))
+print(sum(is.na(df.filtered)))
 
 # Checking the Summary of the data-set (We can see the stats of the data columns eg: mean, median etc.)
-summary(df)
+df.summary = summary(df.filtered)
 
 # Plotting a box plot graph (Box plots are useful to detect potential outliers from the data-set)
-boxplot(df$Comp, ylab = "Comp", main = "Box plot for Compactness")
-boxplot(df$Circ, ylab = "Circ", main = "Box plot for Circularity")
-boxplot(df$D.Circ, ylab = "D.Circ", main = "Box plot for Distance Circularity")
-boxplot(df$Rad.Ra, ylab = "Rad.Ra", main = "Box plot for Radius ratio")
-boxplot(df$Pr.Axis.Ra, ylab = "Pr.Axis.Ra", main = "Box plot for pr.axis aspect ratio")
-boxplot(df$Max.L.Ra, ylab = "Max.L.Ra", main = "Box plot for max.length aspect ratio")
-boxplot(df$Scat.Ra, ylab = "Scat.Ra", main = "Box plot for scatter ratio")
-boxplot(df$Elong, ylab = "Elong", main = "Box plot for elongatedness")
-boxplot(df$Pr.Axis.Rect, ylab = "Pr.Axis.Rect", main = "Box plot for pr.axis rectangularity")
-boxplot(df$Max.L.Rect, ylab = "Max.L.Rect", main = "Box plot for max.length rectangularity")
-boxplot(df$Sc.Var.Maxis, ylab = "Sc.Var.Maxis", main = "Box plot for scaled variance along major axis")
-boxplot(df$Sc.Var.maxis, ylab = "Sc.Var.maxis", main = "Box plot for scaled variance along minor axis")
-boxplot(df$Ra.Gyr, ylab = "Ra.Gyr", main = "Box plot for scaled radius of gyration")
-boxplot(df$Skew.Maxis, ylab = "Ra.Gyr", main = "Box plot for skewness about major axis")
-boxplot(df$Skew.maxis, ylab = "Skew.maxis", main = "Box plot for skewness about minor axis")
-boxplot(df$Kurt.maxis, ylab = "Kurt.maxis", main = "Box plot for kurtosis about minor axis")
-boxplot(df$Kurt.Maxis, ylab = "Kurt.Maxis", main = "Box plot for kurtosis about major axis")
-boxplot(df$Holl.Ra, ylab = "Holl.Ra", main = "Box plot for hollows ratio")
+display.boxplot = function(data, column.name){
+  boxplot(data, ylab = column.name)
+}
 
-# https://www.youtube.com/watch?v=6hRKlZ8D_mk
+# calling the display.boxplot function to display the boxplot data representation for each column.
+display.boxplot(df.filtered$Comp, "Comp")
+display.boxplot(df.filtered$Circ, "Circ")
+display.boxplot(df.filtered$D.Circ, "D.Circ")
+display.boxplot(df.filtered$Rad.Ra, "Rad.Ra")
+display.boxplot(df.filtered$Pr.Axis.Ra, "Pr.Axis.Ra")
+display.boxplot(df.filtered$Max.L.Ra, "Max.L.Ra")
+display.boxplot(df.filtered$Scat.Ra, "Scat.Ra")
+display.boxplot(df.filtered$Elong, "Elong")
+display.boxplot(df.filtered$Pr.Axis.Rect, "Pr.Axis.Rect")
+display.boxplot(df.filtered$Max.L.Rect, "Max.L.Rect")
+display.boxplot(df.filtered$Sc.Var.Maxis, "Sc.Var.Maxis")
+display.boxplot(df.filtered$Sc.Var.maxis, "Sc.Var.maxis")
+display.boxplot(df.filtered$Ra.Gyr, "Ra.Gyr")
+display.boxplot(df.filtered$Skew.Maxis, "Skew.Maxis")
+display.boxplot(df.filtered$Skew.maxis, "Skew.maxis")
+display.boxplot(df.filtered$Kurt.maxis, "Kurt.maxis")
+display.boxplot(df.filtered$Kurt.Maxis, "Kurt.Maxis")
+display.boxplot(df.filtered$Holl.Ra, "Holl.Ra")
 
 
+# Discarding the outliers from the data-set (only for the column which has outliers)
+# any value greater than bench.mark value will be replace with the bench mark value
 
+remove.outliers = function(data, column.name){
+  # filter with box plot and trimming out from 
+  # "maximum": Q3 + 1.5*IQR
+  # "minimum": Q1 -1.5*IQR 
+  # where interquartile range (IQR): 25th to the 75th percentile.
+  
+  # Calculating the upper and lower limit for the data
+  bench.mark.upper = quantile(data, 0.75) + (1.5 * IQR(data))
+  bench.mark.lower = quantile(data, 0.25) - (1.5 * IQR(data))
+  
+  # Replacing the outliers with the upper and lower limits
+  data[data > bench.mark.upper] = bench.mark.upper
+  data[data < bench.mark.lower] = bench.mark.lower
+  
+  # Display the box-plot after removing the outlier
+  display.boxplot(data, column.name)
+  
+}
 
+# calling the remove.outliers function to remove all the outliers from each column of the data
+remove.outliers(df.filtered$Comp, "Comp")
+remove.outliers(df.filtered$Circ, "Circ")
+remove.outliers(df.filtered$D.Circ, "D.Circ")
+remove.outliers(df.filtered$Rad.Ra, "Rad.Ra")
+remove.outliers(df.filtered$Pr.Axis.Ra, "Pr.Axis.Ra")
+remove.outliers(df.filtered$Max.L.Ra, "Max.L.Ra")
+remove.outliers(df.filtered$Scat.Ra, "Scat.Ra")
+remove.outliers(df.filtered$Elong, "Elong")
+remove.outliers(df.filtered$Pr.Axis.Rect, "Pr.Axis.Rect")
+remove.outliers(df.filtered$Max.L.Rect, "Max.L.Rect")
+remove.outliers(df.filtered$Sc.Var.Maxis, "Sc.Var.Maxis")
+remove.outliers(df.filtered$Sc.Var.maxis, "Sc.Var.maxis")
+remove.outliers(df.filtered$Ra.Gyr, "Ra.Gyr")
+remove.outliers(df.filtered$Skew.Maxis, "Skew.Maxis")
+remove.outliers(df.filtered$Skew.maxis, "Skew.maxis")
+remove.outliers(df.filtered$Kurt.maxis, "Kurt.maxis")
+remove.outliers(df.filtered$Kurt.Maxis, "Kurt.Maxis")
+remove.outliers(df.filtered$Holl.Ra, "Holl.Ra")
 
 
 

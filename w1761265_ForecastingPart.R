afee814c916f80c1b211f9061501794f80cf088f
Nazhim <nazhimkalam@gmail.com>
2021-03-28 21:12:52 +0530

    added code links

--- w1761265_ForecastingPart.R
+++ w1761265_ForecastingPart.R
@@ -12,7 +12,17 @@ install.packages("readxl")     # used to read excel data files
 
 # Loading the package
 library(readxl)
+library(ggplot2)
 
 # Reading the data-set "vehicles.xlsx"
 df = read_excel("./GitHub/ML-DM-Course-Work/ExchangeUSD.xlsx")
 View(df)
+plot(df$`USD/EUR`)
+
+# Code References Used
+# https://github.com/alexsnow348/Exchange-Rate-Forecasting-Using-Ensemble-ANN-Model/blob/master/MLP.R
+# https://github.com/EviSfn/MLP-neural-network/blob/master/neuralnetwork.R
+# https://github.com/cran/nnfor/blob/master/R/mlp.R
+
+
+

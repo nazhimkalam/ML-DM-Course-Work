b2c8e6e349edae61a2f9fa3f6276beeef59f467e
Nazhim <nazhimkalam@gmail.com>
2021-04-04 16:31:47 +0530

    commit

--- w1761265_ForecastingPart.R
+++ w1761265_ForecastingPart.R
@@ -27,18 +27,19 @@ library(e1071)
 library(openxlsx)
 library(MLmetrics)
 library(lubridate)
+library(Metrics)
 
 # Reading the data-set "vehicles.xlsx"
 df = read_excel("./GitHub/ML-DM-Course-Work/ExchangeUSD.xlsx")
-View(df)
+# View(df)
 
 # Dropping unwanted columns
 df = subset(df, select = -c(Wdy))
-View(df)
+# View(df)
 
 # Renaming the Columns of the Data-frame
 df = setNames(df, c("Date", "Rate"))
-View(df)
+# View(df)
 
 # Checking for null values present from the dataset
 print(sum(is.na(df)))
@@ -54,28 +55,28 @@ plot(df$Date, df$Rate, main = "Date VS Rate (with outliers)", xlab = "Date",
 boxplot(df$Rate, ylab = "Rate") # Rate has outliers
 boxplot(as.Date(df$Date), ylab = "Date") # Data has no outliers
 
-# # Removing outliers from the "Rate" column dataset
-# 
-# # Calculating the upper and lower limit for the data
-# bench_mark_upper = quantile(df$Rate, 0.75) + (1.5 * IQR(df$Rate))
-# bench_mark_lower = quantile(df$Rate, 0.25) - (1.5 * IQR(df$Rate))
-# 
-# # Replacing the outliers with the upper and lower limits
-# df$Rate[df$Rate > bench_mark_upper] = bench_mark_upper
-# df$Rate[df$Rate < bench_mark_lower] = bench_mark_lower
-# 
-# boxplot(df$Rate, ylab = "Rate") # Rate has outliers
-# 
-# # Plotting the rate vs data graph (without outliers present)
-# plot(df$Date, df$Rate, main = "Date VS Rate (without outliers)", xlab = "Date", ylab = "Rate",
-#      col = "blue", type = "l")
+# Removing outliers from the "Rate" column dataset
+
+# Calculating the upper and lower limit for the data
+bench_mark_upper = quantile(df$Rate, 0.75) + (1.5 * IQR(df$Rate))
+bench_mark_lower = quantile(df$Rate, 0.25) - (1.5 * IQR(df$Rate))
+
+# Replacing the outliers with the upper and lower limits
+df$Rate[df$Rate > bench_mark_upper] = bench_mark_upper
+df$Rate[df$Rate < bench_mark_lower] = bench_mark_lower
+
+boxplot(df$Rate, ylab = "Rate") # Rate has outliers
+
+# Plotting the rate vs data graph (without outliers present)
+plot(df$Date, df$Rate, main = "Date VS Rate (without outliers)", xlab = "Date", ylab = "Rate",
+     col = "blue", type = "l")
 
 # Data Collection
 rates = df[,2]
-View(rates)
+# View(rates)
 
 dates = df[,1]
-View(dates)
+# View(dates)
   
 # min max scalar normalization
 min_rate = min(rates)
@@ -95,18 +96,18 @@ converting_date = function(date_input){
 
 # normalized data
 rates_normalized = data.frame(lapply(rates, normalize))
-View(rates_normalized)
+# View(rates_normalized)
 summary(rates_normalized)
 
 # Converting dates
 all_dates_in_decimal = lapply(dates, converting_date)
 all_dates_in_decimal = lapply(all_dates_in_decimal, normalize)
-View(data.frame(all_dates_in_decimal))
+# View(data.frame(all_dates_in_decimal))
 summary(all_dates_in_decimal)
 
 # Combining data to a data-frame
 final_dataset = data.frame(all_dates_in_decimal, rates_normalized)
-View(final_dataset)
+# View(final_dataset)
 
 # Plotting the Date VS Rate after normalization
 plot(final_dataset$Date, final_dataset$Rate, main = "Date VS Rate (after normalization)",
@@ -114,14 +115,14 @@ plot(final_dataset$Date, final_dataset$Rate, main = "Date VS Rate (after normali
 
 # Creating the Training Data
 training_data = final_dataset[1:400,]
-View(training_data)
+# View(training_data)
 
 # Creating the Testing Data
 testing_data = final_dataset[401:500,]
-View(testing_data)
+# View(testing_data)
 
 # Training a model on the data
-set.seed(1234)
+set.seed(100)
 model <- neuralnet(Rate~.,hidden=c(3),  
                    data = training_data, 
                    act.fct = "logistic", 
@@ -129,11 +130,11 @@ model <- neuralnet(Rate~.,hidden=c(3),
 plot(model)
 
 # Testing the model on the Test dataset
-View(final_dataset)
-testing_data_date = data.frame(final_dataset$Date)
-View(testing_data_date)
-predict_result = predict(model, testing_data_date)
-View(predict_result)
+# View(final_dataset)
+date_dataset = data.frame(final_dataset$Date)
+# View(testing_data_date)
+predict_result = predict(model, date_dataset)
+# View(predict_result)
 
 # un-normalizing the result to get the output rates which can be displayed to the user clearly
 unnormalize = function(x, min, max){
@@ -142,25 +143,25 @@ unnormalize = function(x, min, max){
 
 # un-normalized predicted rate result
 unnormalized_predicted_result = unnormalize(predict_result, min_rate, max_rate)
-View(unnormalized_predicted_result)
+# View(unnormalized_predicted_result)
 
 # un-normalized actual rate result
-unnormalized_testing_dataset = unnormalize(data.frame(final_dataset$Rate), min_rate, max_rate)
-View(unnormalized_testing_dataset)
+unnormalized_actual_result = unnormalize(data.frame(final_dataset$Rate), min_rate, max_rate)
+# View(unnormalized_actual_result)
 
 # Combining the predicted rate dataset and actual rate dataset
-combined_rates = data.frame(unnormalized_predicted_result, unnormalized_testing_dataset)
+combined_rates = data.frame(unnormalized_predicted_result, unnormalized_actual_result)
 combined_rates = setNames(combined_rates, c("Predicted Rates", "Actual Rates"))
-View(combined_rates)
+# View(combined_rates)
 
 # plotting Actual Rate VS Predicted Rate result
-plot(testing_data_date$final_dataset.Date, unnormalized_testing_dataset$final_dataset.Rate,
+plot(date_dataset$final_dataset.Date, unnormalized_actual_result$final_dataset.Rate,
      main = "Actual VS Predicted",
      xlab = "Date", ylab = "Rate", col = "orange", type="l")
-lines(testing_data_date$final_dataset.Date,unnormalized_predicted_result,col="green")
-x = testing_data_date[401:500,]
-y = unnormalized_predicted_result[401:500,]
-lines(x,y,col="red")
+lines(date_dataset$final_dataset.Date,unnormalized_predicted_result,col="green")
+testing_data_date = date_dataset[401:500,]
+predicted_data_testing_rate = unnormalized_predicted_result[401:500,]
+lines(testing_data_date, predicted_data_testing_rate, col="red")
 
 # Evaluation model performance
 

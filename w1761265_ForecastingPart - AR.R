969e1c1430b6f8889ca89db9b0188a86760c8710
Nazhim <nazhimkalam@gmail.com>
2021-04-16 11:22:37 +0530

    commit

--- w1761265_ForecastingPart - AR.R
+++ w1761265_ForecastingPart - AR.R
@@ -1,3 +1,27 @@
+# --------------------------------------------------
+# Name: Mohammed Nazhim Kalam
+# Student ID: 2019281
+# UoW ID: W1761265
+# --------------------------------------------------
+
+# FORECASTING PART 
+# Installing package to read Excel Data-set
+install.packages("fpp")
+install.packages("MASS")
+install.packages("readxl")
+install.packages("neuralnet")
+install.packages("ggplot2")
+install.packages("reshape2")
+install.packages("gridExtra")
+install.packages("fpp2")
+install.packages("e1071")
+install.packages("openxlsx")
+install.packages("MLmetrics")
+install.packages("lubridate")
+install.packages("Metrics")
+install.packages("tidyr")
+install.packages("graphics")
+
 # Loading the package
 library(fpp)
 library(MASS)
@@ -30,6 +54,19 @@ df_copy = df
 # Renaming the Columns of the Data-frame
 df = setNames(df, c("Rate_Original", "Rate_Lag"))
 View(df)
+
+# Variables used for 1 hidden and 2 hidden layers for the neural network
+# NOTE: One of the two sets have to be commented while the other set is used for the model training
+
+# THIS SET OF VARIABLES IS USED FOR 1 HIDDEN LAYER MLP
+# HIDDEN_LAYERS = c(6)
+# ACTIVATION_FUNCTION = "logistic"
+# LEARNING_RATE = 0.12
+
+# THIS SET OF VARIABLES ARE USED FOR THE 2 HIDDEN LAYER MLP
+HIDDEN_LAYERS = c(6,6)
+ACTIVATION_FUNCTION = "logistic"
+LEARNING_RATE = 0.08
 
 # Looping the AR Order from 1 to 10 to get the one which performs the best
 for (index in 1:10) { 
@@ -64,15 +101,77 @@ for (index in 1:10) {
   # Training a model on the data
   set.seed(101)
   
+  # USED FOR TUNING THE NEURAL NET PARAMETERS IN ORDER TO GET THE BEST SET OF PARAMETERS FOR THE MODEL
+  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY FOR GETTING THE BEST SET OF NODES
+  # Loop from 1 to 10 node for one and two hidden layer to get the best node number for each layer
+  # for (x in 1:10) {
+  #   print("-----------------------------------------")
+  #   model <- neuralnet(Rate~.,
+  #                      # hidden=c(x),
+  #                      hidden=c(6,x),
+  #                      data = training_data,
+  #                      act.fct = "logistic",
+  #                      linear.output = TRUE,
+  #                      err.fct = "sse",
+  #                      lifesign = "full",
+  #                      rep = 10,
+  #                      learningrate = 0.1)
+  # }
+  
+  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO GET THE BEST LEARNING RATE
+  # Looping to get the best learning rate which gives the least amount of error rate for the 
+  # model (this includes 1 layer and 2 layer model with 6 nodes for both layers since that 
+  # is the optimal number of nodes found previously)
+  # learning_rate = 0
+  # while (learning_rate <= 1) {
+  #   learning_rate = learning_rate + 0.01
+  #   print(learning_rate)
+  #   model <- neuralnet(Rate~.,
+  #                      # hidden=c(6), 
+  #                      hidden=c(6,6),
+  #                      data = training_data, 
+  #                      act.fct = "logistic", 
+  #                      linear.output = TRUE,
+  #                      err.fct = "sse",
+  #                      lifesign = "full",
+  #                      learningrate = learning_rate)
+  # }
+  
+  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO CHECK WHICH ACTIVATION FUNCTION
+  # PERFORMS BEST WITH THE FOUND BEST NODES AND LEARNING RATE VALUES
+  # activationFunction = "logistic"
+  # activationFunction = "tanh"
+  # model <- neuralnet(Rate~.,
+  #            # hidden=c(6),
+  #            hidden=c(6,6),
+  #            data = training_data,
+  #            act.fct = activationFunction,
+  #            linear.output = TRUE,
+  #            err.fct = "sse",
+  #            lifesign = "full",
+  #            learningrate = 0.08)
+  # }
+  
+  # with one hidden layer and 6 nodes & two hidden layer with 6,6 nodes for the layers
+  
+  # OPTIMUM RESULT FOR 1 HIDDEN LAYER MLP
+  # 6 NODES
+  # LEARNING RATE = 0.12
+  # ACTIVATION FUNCTION = LOGISTIC
+  
+  # OPTIMUM RESULT FOR 2 HIDDEN LAYER MLP
+  # 6 6 NODES
+  # LEARNING RATE = 0.08
+  # ACTIVATION FUNCTION = LOGISTIC
+  
   # Training the model
   model <- neuralnet(Rate_Original~Rate_Lag,
-                     # hidden=c(6),
-                     hidden=c(6,6),
+                     hidden = HIDDEN_LAYERS,
                      data = training_data, 
-                     act.fct = "logistic", 
+                     act.fct = ACTIVATION_FUNCTION, 
                      linear.output = TRUE,
                      err.fct = "sse", 
-                     learningrate = 0.08)
+                     learningrate = LEARNING_RATE)
   
   # testing_data_actual_rate = data.frame(testing_data)
   predict_result = predict(model, testing_data)
@@ -82,7 +181,7 @@ for (index in 1:10) {
   actual = data.frame(testing_data)
   predicted = predict_result
   
-  # Evluation for the AR order number
+  # Evaluation for the AR order number
   print("------------------------------------------")
   print(paste("Evaluation for the AR Order:", index))
   
@@ -99,12 +198,56 @@ for (index in 1:10) {
   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
 }
 
+# Since AR1 gave the best result we will be using that
+# Using the saved dataframe copy 
+df = df_copy
+
+# Renaming the Columns of the Data-frame
+df = setNames(df, c("Rate_Original", "Rate_Lag"))
+
+# Shifting the Rate_Lag column rows by one down below
+df['Rate_Lag'] <- c(NA, head(df['Rate_Lag'], dim(df)[1] - 1)[[1]])
+
+# Removing the first row from the dataframe because there is a null value present in the Rate_Lag column
+df = drop_na(df)
+
+# normalization
+normalize = function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+# normalized data
+df.normalized = data.frame(lapply(df, normalize))
+View(df.normalized)
+
+# Creating the Training Data
+training_data = df.normalized[1:400,]
+
+# Creating the Testing Data
+testing_data = df.normalized[401:500-1,]
+View(testing_data)
+
+# Training a model on the data
+set.seed(101)
+
+# Training the model
+model <- neuralnet(Rate_Original~Rate_Lag,
+                   hidden = HIDDEN_LAYERS,
+                   data = training_data, 
+                   act.fct = ACTIVATION_FUNCTION, 
+                   linear.output = TRUE,
+                   err.fct = "sse", 
+                   learningrate = LEARNING_RATE)
+
+# testing_data_actual_rate = data.frame(testing_data)
+predict_result = predict(model, testing_data)
+
 # Adding the index column
 testing_data$Index = seq.int(nrow(testing_data))
 
 # Plotting the graph
 plot(testing_data$Index,testing_data$Rate_Original, 
-     main = "Actual VS Predicted (using AR1 on Testing Data)", xlab = "Index",
+     main = "Actual VS Predicted", xlab = "Index",
      ylab = "Rate", col = "black", type = "l")
 lines(testing_data$Index, predict_result, col="red") 
 legend("bottomright",                    # Add legend to plot

7c88c7e5213e57ebf41dffd60cf8da89768708b5
Nazhim <nazhimkalam@gmail.com>
2021-04-26 18:00:11 +0530

    added pacf graph

--- w1761265_ForecastingPart - AR.R
+++ w1761265_ForecastingPart - AR.R
@@ -30,6 +30,7 @@ library(neuralnet)
 library(ggplot2)
 library(reshape2)
 library(gridExtra)
+library(tseries)
 library(fpp2)
 library(e1071)
 library(openxlsx)
@@ -59,9 +60,14 @@ df_copy = df
 df = setNames(df, c("Rate_Original", "Rate_Lag"))
 View(df)
 
+# Performing 'pacf', Partial Autocorrelation Function
+rate = df$Rate_Original
+pacf (rate, lag = 10)
+
 # Variables used for 1 hidden and 2 hidden layers for the neural network
 # NOTE: One of the two sets have to be commented while the other set is used for the model training
 
+# -------------------------------------------------------------------
 # # THIS SET OF VARIABLES IS USED FOR 1 HIDDEN LAYER MLP
 # HIDDEN_LAYERS = c(6)
 # ACTIVATION_FUNCTION = "logistic"
@@ -71,6 +77,7 @@ View(df)
 HIDDEN_LAYERS = c(6,6)
 ACTIVATION_FUNCTION = "logistic"
 LEARNING_RATE = 0.08
+# -------------------------------------------------------------------
 
 # Looping the AR Order from 1 to 10 to get the one which performs the best
 for (index in 1:10) { 
@@ -107,106 +114,6 @@ for (index in 1:10) {
   # Training a model on the data
   set.seed(101)
   
-  # USED FOR TUNING THE NEURAL NET PARAMETERS IN ORDER TO GET THE BEST SET OF PARAMETERS FOR THE MODEL
-  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY FOR GETTING THE BEST SET OF NODES
-  # Loop from 1 to 10 node for one and two hidden layer to get the best node number for each layer
-  # for (x in 1:10) {
-  #   print("-----------------------------------------")
-  #   model <- neuralnet(Rate_Original~Rate_Lag,
-  #                      hidden=c(x),
-  #                      # hidden=c(6,x),
-  #                      data = training_data,
-  #                      act.fct = "logistic",
-  #                      linear.output = TRUE,
-  #                      err.fct = "sse",
-  #                      learningrate = 0.1)
-  #   
-  #   predict_result = predict(model, testing_data)
-  #   View(predict_result)
-  #   
-  #   # Evaluating the model
-  #   actual = data.frame(testing_data)
-  #   predicted = predict_result
-  #   
-  #   print(paste("Nodes:", x))
-  #   # Calculating the Mean Absolute Error
-  #   mae = round(mae(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Mean Absolute Error: ", mae, " %", sep = ""))
-  #   
-  #   # Calculating the Root Mean Squared Error
-  #   rmse = round(rmse(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Root Mean Squared Error: ", rmse, " %", sep = ""))
-  #   
-  #   # Calculating the Mean Absolute Percentage Error Loss
-  #   mape = round(MAPE(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
-  # }
-  
-  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO GET THE BEST LEARNING RATE
-  # Looping to get the best learning rate which gives the least amount of error rate for the 
-  # model (this includes 1 layer and 2 layer model with 6 nodes for both layers since that 
-  # is the optimal number of nodes found previously)
-  # learning_rate = 0
-  # while (learning_rate <= 0.1) {
-  #   learning_rate = learning_rate + 0.02
-  #   print(learning_rate)
-  #   model <- neuralnet(Rate_Original~Rate_Lag,
-  #                      hidden=c(6),
-  #                      # hidden=c(6,6),
-  #                      data = training_data,
-  #                      act.fct = "logistic",
-  #                      linear.output = TRUE,
-  #                      err.fct = "sse",
-  #                      learningrate = learning_rate)
-  #   
-  #   predict_result = predict(model, testing_data)
-  #   View(predict_result)
-  #   
-  #   # Evaluating the model
-  #   actual = data.frame(testing_data)
-  #   predicted = predict_result
-  #   
-  #   print(paste("learning_rate:", learning_rate))
-  #   # Calculating the Mean Absolute Error
-  #   mae = round(mae(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Mean Absolute Error: ", mae, " %", sep = ""))
-  #   
-  #   # Calculating the Root Mean Squared Error
-  #   rmse = round(rmse(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Root Mean Squared Error: ", rmse, " %", sep = ""))
-  #   
-  #   # Calculating the Mean Absolute Percentage Error Loss
-  #   mape = round(MAPE(actual$Rate_Original, predicted) * 100, digits = 4)
-  #   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
-  # }  
-  
-  # PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO CHECK WHICH ACTIVATION FUNCTION
-  # PERFORMS BEST WITH THE FOUND BEST NODES AND LEARNING RATE VALUES
-  # activationFunction = "logistic"
-  # activationFunction = "tanh"
-  # model <- neuralnet(Rate~.,
-  #            # hidden=c(6),
-  #            hidden=c(6,6),
-  #            data = training_data,
-  #            act.fct = activationFunction,
-  #            linear.output = TRUE,
-  #            err.fct = "sse",
-  #            lifesign = "full",
-  #            learningrate = 0.1)
-  # }
-  
-  # with one hidden layer and 6 nodes & two hidden layer with 6,6 nodes for the layers
-  
-  # OPTIMUM RESULT FOR 1 HIDDEN LAYER MLP
-  # 6 NODES
-  # LEARNING RATE = 0.1
-  # ACTIVATION FUNCTION = LOGISTIC
-  
-  # OPTIMUM RESULT FOR 2 HIDDEN LAYER MLP
-  # 6 6 NODES
-  # LEARNING RATE = 0.08
-  # ACTIVATION FUNCTION = LOGISTIC
-  
   # Training the model
   model <- neuralnet(Rate_Original~Rate_Lag,
                      hidden = HIDDEN_LAYERS,
@@ -240,6 +147,106 @@ for (index in 1:10) {
   mape = round(MAPE(actual$Rate_Original, predicted) * 100, digits = 4)
   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
 }
+
+# USED FOR TUNING THE NEURAL NET PARAMETERS IN ORDER TO GET THE BEST SET OF PARAMETERS FOR THE MODEL
+# PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY FOR GETTING THE BEST SET OF NODES
+# Loop from 1 to 10 node for one and two hidden layer to get the best node number for each layer
+# for (x in 1:10) {
+#   print("-----------------------------------------")
+#   model <- neuralnet(Rate_Original~Rate_Lag,
+#                      hidden=c(x),
+#                      # hidden=c(6,x),
+#                      data = training_data,
+#                      act.fct = "logistic",
+#                      linear.output = TRUE,
+#                      err.fct = "sse",
+#                      learningrate = 0.1)
+#   
+#   predict_result = predict(model, testing_data)
+#   View(predict_result)
+#   
+#   # Evaluating the model
+#   actual = data.frame(testing_data)
+#   predicted = predict_result
+#   
+#   print(paste("Nodes:", x))
+#   # Calculating the Mean Absolute Error
+#   mae = round(mae(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Mean Absolute Error: ", mae, " %", sep = ""))
+#   
+#   # Calculating the Root Mean Squared Error
+#   rmse = round(rmse(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Root Mean Squared Error: ", rmse, " %", sep = ""))
+#   
+#   # Calculating the Mean Absolute Percentage Error Loss
+#   mape = round(MAPE(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
+# }
+
+# PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO GET THE BEST LEARNING RATE
+# Looping to get the best learning rate which gives the least amount of error rate for the 
+# model (this includes 1 layer and 2 layer model with 6 nodes for both layers since that 
+# is the optimal number of nodes found previously)
+# learning_rate = 0
+# while (learning_rate <= 0.1) {
+#   learning_rate = learning_rate + 0.02
+#   print(learning_rate)
+#   model <- neuralnet(Rate_Original~Rate_Lag,
+#                      hidden=c(6),
+#                      # hidden=c(6,6),
+#                      data = training_data,
+#                      act.fct = "logistic",
+#                      linear.output = TRUE,
+#                      err.fct = "sse",
+#                      learningrate = learning_rate)
+#   
+#   predict_result = predict(model, testing_data)
+#   View(predict_result)
+#   
+#   # Evaluating the model
+#   actual = data.frame(testing_data)
+#   predicted = predict_result
+#   
+#   print(paste("learning_rate:", learning_rate))
+#   # Calculating the Mean Absolute Error
+#   mae = round(mae(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Mean Absolute Error: ", mae, " %", sep = ""))
+#   
+#   # Calculating the Root Mean Squared Error
+#   rmse = round(rmse(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Root Mean Squared Error: ", rmse, " %", sep = ""))
+#   
+#   # Calculating the Mean Absolute Percentage Error Loss
+#   mape = round(MAPE(actual$Rate_Original, predicted) * 100, digits = 4)
+#   print(paste("Mean Absolute Percentage Error Loss: ", mape, " %", sep = ""))
+# }  
+
+# PLEASE DONT UN-COMMENT AND RUN THIS BECAUSE THIS WAS USED ONLY TO CHECK WHICH ACTIVATION FUNCTION
+# PERFORMS BEST WITH THE FOUND BEST NODES AND LEARNING RATE VALUES
+# activationFunction = "logistic"
+# activationFunction = "tanh"
+# model <- neuralnet(Rate~.,
+#            # hidden=c(6),
+#            hidden=c(6,6),
+#            data = training_data,
+#            act.fct = activationFunction,
+#            linear.output = TRUE,
+#            err.fct = "sse",
+#            lifesign = "full",
+#            learningrate = 0.1)
+# }
+
+# with one hidden layer and 6 nodes & two hidden layer with 6,6 nodes for the layers
+
+# OPTIMUM RESULT FOR 1 HIDDEN LAYER MLP
+# 6 NODES
+# LEARNING RATE = 0.1
+# ACTIVATION FUNCTION = LOGISTIC
+
+# OPTIMUM RESULT FOR 2 HIDDEN LAYER MLP
+# 6 6 NODES
+# LEARNING RATE = 0.08
+# ACTIVATION FUNCTION = LOGISTIC
 
 # Since AR1 gave the best result we will be using that
 # Using the saved dataframe copy 
